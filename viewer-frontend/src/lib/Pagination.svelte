88bcb6512c387069f5a4e88ad8f8fc244d6c28f4
Lexi <lexi@qixils.dev>
2022-01-20 01:43:33 -0500

    Mark pagination buttons as aria-disabled as necessary

--- viewer-frontend/src/lib/Pagination.svelte
+++ viewer-frontend/src/lib/Pagination.svelte
@@ -26,7 +26,7 @@
         </a>
       </li>
     {:else}
-      <li>
+      <li aria-disabled="true">
         <span class="block py-2 px-2 ml-0 leading-tight text-gray-300 bg-gray-100 rounded-l-lg border border-gray-300 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-700">
           <span class="sr-only">Previous (Unavailable)</span>
           <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -54,12 +54,12 @@
         </a>
       </li>
     {:else}
-        <li>
-          <span class="block py-2 px-2 ml-0 leading-tight text-gray-300 bg-gray-100 rounded-l-lg border border-gray-300 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-700">
-            <span class="sr-only">Next (Unavailable)</span>
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </span>
-        </li>
+      <li aria-disabled="true">
+        <span class="block py-2 px-2 ml-0 leading-tight text-gray-300 bg-gray-100 rounded-l-lg border border-gray-300 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-700">
+          <span class="sr-only">Next (Unavailable)</span>
+          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </span>
+      </li>
     {/if}
 
   </ul>
